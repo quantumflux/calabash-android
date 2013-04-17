745a1604b446eb43cf0d963e3b2b5edc968a5fd1
quantumflux <github@celestialspheres.co.uk>
2013-04-18 00:31:05 +0100

    Added different action file

---
+++ ruby-gem/test-server/instrumentation-backend/src/sh/calaba/instrumentationbackend/actions/list/We7ClickTabFragmentListItem.java
@@ -0,0 +1,51 @@
+package sh.calaba.instrumentationbackend.actions.list;
+
+import java.util.ArrayList;
+
+import sh.calaba.instrumentationbackend.InstrumentationBackend;
+import sh.calaba.instrumentationbackend.Result;
+import sh.calaba.instrumentationbackend.actions.Action;
+import android.widget.ListView;
+import android.widget.TabHost;
+import android.widget.TextView;
+
+public class We7ClickTabFragmentListItem implements Action {
+
+  @Override
+  public Result execute(String... args) {
+
+    int tabIndex = Integer.parseInt(args[0]);
+
+    int listItemIndex = Integer.parseInt((args[1]));
+
+    // Get tabhost view
+
+    TabHost tabHost = (TabHost) InstrumentationBackend.solo.getView(TabHost.class, 0);
+
+    if (tabIndex >= tabHost.getTabWidget().getTabCount())  {
+
+      return new Result(false, "no tab at index: " + tabIndex);
+
+    }
+
+    tabHost.setCurrentTab(tabIndex);
+
+    ListView listView = (ListView) InstrumentationBackend.solo.getView(ListView.class, tabIndex);
+
+    ArrayList<TextView> listTextViews = InstrumentationBackend.solo.clickInList(listItemIndex, tabIndex);
+
+    StringBuilder sb = new StringBuilder();
+    for (TextView tv : listTextViews) {
+      sb.append(tv.getText() + ",");
+    }
+
+    return new Result(true, sb.toString());
+
+  }
+
+  @Override
+  public String key() {
+    return "click_tab_list_item";
+  }
+
+}
--- ruby-gem/test-server/instrumentation-backend/src/sh/calaba/instrumentationbackend/actions/wait/We7WaitForHomeButtonAvailable.java
+++ ruby-gem/test-server/instrumentation-backend/src/sh/calaba/instrumentationbackend/actions/wait/We7WaitForHomeButtonAvailable.java
@@ -1,77 +1,75 @@
-package sh.calaba.instrumentationbackend.actions.view;
+package sh.calaba.instrumentationbackend.actions.wait;
 
+import java.util.HashMap;
+
+import sh.calaba.instrumentationbackend.InstrumentationBackend;
 import sh.calaba.instrumentationbackend.Result;
 import sh.calaba.instrumentationbackend.TestHelpers;
 import sh.calaba.instrumentationbackend.actions.Action;
-import sh.calaba.instrumentationbackend.InstrumentationBackend;
-
-import java.util.HashMap;
 import android.view.View;
-import android.graphics.ColorFilter;
-import java.lang.reflect.Field;
 
 public class We7WaitForHomeButtonAvailable implements Action {
 
-    // This MUST be the same value as set in com.we7.player.ui.fragment.We7Fragment.CONTENT_AVAILABILITY_KEY
-    public static final String CONTENT_AVAILABILITY_KEY = "available";
-    
-    @Override
-    public Result execute(String... args) {
-        
-        final String firstArgument = args[0];
-        final View foundView = TestHelpers.getViewById(firstArgument);
-        
-        if( null == foundView) {
-            return notFoundResult(firstArgument);
-        }
-       
-        HashMap<String, Object> tagMap = (HashMap<String, Object>) foundView.getTag();
-        
-        if (null == tagMap) {
-            return new Result(false, "No availability tag set on home button");
-        }
-        
-        int timeout = 90 * 1000;
-        if (args.length > 1) { // a second argument is a timeout
-            try {
-                // the argument is in seconds but robotium takes milliseconds
-                timeout = 1000 * Integer.parseInt(args[1]);
-            } catch (NumberFormatException e) {
-                return new Result(false, "Invalid timeout supplied. Should be an integer.");
-            }
-        }
-        
-        long endTime = System.currentTimeMillis() + timeout;
-        while (System.currentTimeMillis() < endTime) {
-            
-            if ((Boolean) tagMap.get(CONTENT_AVAILABILITY_KEY)) {
-                
-               return Result.successResult();
-                
-           } else {
-               
-               try {
-                   Thread.sleep(500);
-               } catch (InterruptedException e) {
-                   return Result.fromThrowable(e);
-               }
-               
-           }
-            
-        }
-        return new Result(false, "Timed out while waiting for home button :'" + firstArgument + "' to become available");
-        
+  // This MUST be the same value as set in com.we7.player.ui.fragment.We7Fragment.CONTENT_AVAILABILITY_KEY
+  public static final String CONTENT_AVAILABILITY_KEY = "available";
+
+  @Override
+  public Result execute(String... args) {
+
+    final String firstArgument = args[0];
+    final View foundView = TestHelpers.getViewById(firstArgument);
+
+    if( null == foundView) {
+      return notFoundResult(firstArgument);
     }
 
-    @Override
-    public String key() {
-                
-        InstrumentationBackend.log("returning key name wait_for_home_button_available");
-                
-        return "wait_for_home_button_available";
+    HashMap<String, Object> tagMap = (HashMap<String, Object>) foundView.getTag();
+
+    if (null == tagMap) {
+      return new Result(false, "No availability tag set on home button");
     }
 
-    private Result notFoundResult(final String firstArgument) {
-        return Result.failedResult(String.format("Home button with id %s was not found", firstArgument));
+    int timeout = 90 * 1000;
+    if (args.length > 1) { // a second argument is a timeout
+      try {
+        // the argument is in seconds but robotium takes milliseconds
+        timeout = 1000 * Integer.parseInt(args[1]);
+      } catch (NumberFormatException e) {
+        return new Result(false, "Invalid timeout supplied. Should be an integer.");
+      }
     }
+
+    long endTime = System.currentTimeMillis() + timeout;
+    while (System.currentTimeMillis() < endTime) {
+
+      if ((Boolean) tagMap.get(CONTENT_AVAILABILITY_KEY)) {
+
+        return Result.successResult();
+
+      } else {
+
+        try {
+          Thread.sleep(500);
+        } catch (InterruptedException e) {
+          return Result.fromThrowable(e);
+        }
+
+      }
+
+    }
+    return new Result(false, "Timed out while waiting for home button :'" + firstArgument + "' to become available");
+
+  }
+
+  @Override
+  public String key() {
+
+    InstrumentationBackend.log("returning key name wait_for_home_button_available");
+
+    return "wait_for_home_button_available";
+  }
+
+  private Result notFoundResult(final String firstArgument) {
+    return Result.failedResult(String.format("Home button with id %s was not found", firstArgument));
+  }
 }
