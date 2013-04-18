fd8bfd183080b17e14baf1cda888889c57135390
Alister Cooper <alister@we7.com>
2013-04-18 13:57:55 +0100

    Modified We7ClickTabFragmentListItem.java to do a bit more error checking and to handle assert exceptions that may occur while the list is being updated

--- ruby-gem/test-server/instrumentation-backend/src/sh/calaba/instrumentationbackend/actions/list/We7ClickTabFragmentListItem.java
+++ ruby-gem/test-server/instrumentation-backend/src/sh/calaba/instrumentationbackend/actions/list/We7ClickTabFragmentListItem.java
@@ -5,39 +5,62 @@ import java.util.ArrayList;
 import sh.calaba.instrumentationbackend.InstrumentationBackend;
 import sh.calaba.instrumentationbackend.Result;
 import sh.calaba.instrumentationbackend.actions.Action;
-import android.widget.ListView;
 import android.widget.TabHost;
 import android.widget.TextView;
 
 public class We7ClickTabFragmentListItem implements Action {
 
   @Override
-  public Result execute(String... args) {
+  public Result execute(final String... args) {
 
     int tabIndex = Integer.parseInt(args[0]);
 
     int listItemIndex = Integer.parseInt((args[1]));
 
-    // Get tabhost view
-
     TabHost tabHost = (TabHost) InstrumentationBackend.solo.getView(TabHost.class, 0);
 
-    if (tabIndex >= tabHost.getTabWidget().getTabCount())  {
-
+    // Check valid index
+    if (tabIndex < 0 || tabIndex >= tabHost.getTabWidget().getTabCount())  {
       return new Result(false, "no tab at index: " + tabIndex);
+    }
+
+    // Check tab is showing
+    if (tabHost.getCurrentTab() != tabIndex) {
+      return new Result(false, "tab " + tabIndex + " is not current tab (" + tabHost.getCurrentTab() + ")");
+    }
+
+    long startTime = System.currentTimeMillis();
+    long timeOut = 90 * 1000;
+    Result result = null;
+
+    Exception lastException = null;
+
+    while (System.currentTimeMillis() - startTime <= timeOut && result == null) {
+
+      try {
+
+        ArrayList<TextView> listTextViews = InstrumentationBackend.solo.clickInList(listItemIndex, tabIndex);
+
+        StringBuilder sb = new StringBuilder();
+        for (TextView tv : listTextViews) {
+          sb.append(tv.getText() + ",");
+        }
+
+        result = new Result(true, sb.toString() + (lastException == null ? "":" /nAn exception was thrown: " + lastException.getMessage() + " /n" + lastException.getStackTrace()));
+
+      } catch (Exception e) {
+
+        lastException = e;
+
+      }
 
     }
 
-    ListView listView = (ListView) InstrumentationBackend.solo.getView(ListView.class, tabIndex);
-
-    ArrayList<TextView> listTextViews = InstrumentationBackend.solo.clickInList(listItemIndex, tabIndex);
-
-    StringBuilder sb = new StringBuilder();
-    for (TextView tv : listTextViews) {
-      sb.append(tv.getText() + ",");
+    if (result == null) {
+      result = new Result(false, (lastException == null ? "":" /nAn exception was thrown: " + lastException.getMessage() + " /n" + lastException.getStackTrace()));
     }
 
-    return new Result(true, sb.toString());
+    return result;
 
   }
 
