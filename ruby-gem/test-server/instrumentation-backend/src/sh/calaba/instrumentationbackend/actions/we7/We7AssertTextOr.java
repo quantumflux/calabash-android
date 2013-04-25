77f58c68d83450fab4a86d2f0eaec0fc5d6e290f
Alister Cooper <alister@we7.com>
2013-04-25 15:58:28 +0100

    A couple of Actions to allow search for text1 or text2 and to click same

---
+++ ruby-gem/test-server/instrumentation-backend/src/sh/calaba/instrumentationbackend/actions/we7/We7AssertTextOr.java
@@ -0,0 +1,42 @@
+package sh.calaba.instrumentationbackend.actions.we7;
+
+
+import java.util.ArrayList;
+
+import sh.calaba.instrumentationbackend.InstrumentationBackend;
+import sh.calaba.instrumentationbackend.Result;
+import sh.calaba.instrumentationbackend.actions.Action;
+
+
+public class We7AssertTextOr extends We7Action implements Action{
+
+    @Override
+    public Result execute(String... args) {
+        
+    	ArrayList<String> textToOr = new ArrayList<String>();
+    	String textList = "";
+    	for (int i = 0; i < args.length; i++) {
+    		
+    		textToOr.add(args[i]);
+    		textList += args[i] + ",";
+    		
+    	}
+
+        for (String text: textToOr) {
+        	
+        	if (InstrumentationBackend.solo.searchText(text, true)) {
+        		return new Result(true, "Text '" + text + "' was found");
+        	}
+        	
+        }
+        
+        return new Result(false, "None of: " + textList + "' were found");
+
+    }
+
+    @Override
+    public String key() {
+        return "assert_text_or";
+    }
+
+}
